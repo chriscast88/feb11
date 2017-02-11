d5f1659935a2e1e45791be0c89eddd7db3584267
chriscast88 <christian.castaneda@gmail.com>
2017-02-11 13:44:51 -0800

    commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = window.location.search.match(/print-pdf/gi) ? 'css/print/pdf.css' : 'css/print/paper.css';
+document.getElementsByTagName('head')[0].appendChild(link);
+
+Reveal.initialize({
+			controls: true,
+			progress: true,
+			history: true,
+			center: true,
+
+			transition: 'slide' });
